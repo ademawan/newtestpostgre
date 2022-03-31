532baaac3a8ac8c8d443f8f4aaa613ec622cba80
Ade Mawan <ademawan1210@gmail.com>
2022-03-31 22:47:48 +0700

    ssssddssd

--- utils/mysqldriver.go
+++ utils/mysqldriver.go
@@ -11,9 +11,10 @@ import (
 )
 
 func InitDB(config *configs.AppConfig) *gorm.DB {
+	//postgres://nmqdckadrkgkvn:3431d43e0c453af757a272d2d3c940180f96fb115bcbc25a7e5f3ff8743fa090@ec2-3-229-161-70.compute-1.amazonaws.com:5432/ddb7g4irblf0rr
 
-	DBURL := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable TimeZone=Asia/Jakarta", config.Database.Username, config.Database.Password, config.Database.Address, config.Database.Port, config.Database.Name)
-	db, err := gorm.Open(postgres.Open(DBURL), &gorm.Config{})
+	DBURL := fmt.Sprintf("postgres://%s:%v@%s:%s/%s", config.Database.Username, config.Database.Password, config.Database.Address, config.Database.Port, config.Database.Name)
+	db, err := gorm.Open(DBURL), &gorm.Config{})
 	if err != nil {
 		fmt.Printf("Cannot connect to database ")
 	} else {
